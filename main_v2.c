78e7b958157393f1f46180c4207dadc24fe9394b
kuzeytunakale <kuzeytunakale@gmail.com>
2026-08-25 19:56:46 +0300

    yeni

--- main_v2.c
+++ main_v2.c
@@ -3,8 +3,9 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    int capacity = 2;
+    int capacity = 0; // Her iihtimale karşı 1 den başlattım
     int karakter;
+    int en_uzun_satirin_boyu = 0;
     FILE * fPtr;
 
     // Dosya varsa okuma yazma moduyla açar, dosya yoksa yaratmak için yazma moduyla açar.
@@ -16,6 +17,18 @@ int main(int argc, char *argv[]) {
             return 1;
         }
     }
+    int max = 0;
+    while ((karakter = fgetc(fPtr)) != EOF) {
+        max++;
+        if ((karakter == '\0') || (karakter == '\n')) {
+            capacity++;
+            if (max > en_uzun_satirin_boyu) {
+                en_uzun_satirin_boyu = max;
+            }
+        }
+    }
+
+    char satirsatir[en_uzun_satirin_boyu];
 
     // 1. Metin adreslerini (char*) tutacak dinamik pointer dizisi
     char **Metin = malloc(capacity * sizeof(char *));
@@ -24,21 +37,34 @@ int main(int argc, char *argv[]) {
         return(1);
     }
 
+    int i = 0;
+    while ((karakter = fgetc(fPtr)) != EOF) {
+    strcat(satirsatir, karakter)
+    Metin[i] = strdup("İlk satır metni");
+        
+    }
+
+
+
+
+
+
+    //listedeli belleklerin initilazyonu
     for (int i = 0; i < capacity; i++) {
         Metin[i] = malloc(50 * sizeof(char));
         if (Metin[i] == NULL) {
             printf("Hata: Metin %d icin bellek ayrilamadi!\n", i + 1);
-             // O ana kadar ayrılmış bellekleri temizleyip çıkmalıyız
+            // O ana kadar ayrılmış bellekleri temizleyip çıkmalıyız
             for (int j = 0; j < i; j++) free(Metin[j]);
             free(Metin);
             return 1;
         }
     }
 
-    for (int index = 0; index < capacity; index++) {
-        while ((karakter = fgetc(fPtr)) != EOF) {
+    while ((karakter = fgetc(fPtr)) != EOF) {
+        for (int index = 0; index < capacity; index++) {
             Metin[index] 
         }
-
     }
+    
 }
