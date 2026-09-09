ccaeb0b3215d23e62341166ff8389f111127e32a
Kuzey T. Kale <166996695+kuzeytunakale@users.noreply.github.com>
2026-09-09 14:36:39 +0300

    Update main_v2.c

--- main_v2.c
+++ main_v2.c
@@ -3,9 +3,35 @@
 #include <string.h>
 #include <locale.h>
 #include <ncurses.h>
+#include <strings.h>
+#include <wchar.h>
+#include <unistd.h>
+
+#define CTRL_S 19
+#define CTRL_Q 17
 
 // dosyanın zaten olduğunu varsayarak bunu yaptım, hata !!!!
 
+
+// Ekrandaki karakter indeksini (x) bellekteki bayt indeksine çevirir
+int char_to_byte(const char *str, int char_pos) {
+    int b = 0, c = 0;
+    while (str[b] != '\0' && c < char_pos) {
+        unsigned char ch = str[b];
+        if ((ch & 0x80) == 0) b += 1;
+        else if ((ch & 0xE0) == 0xC0) b += 2;
+        else if ((ch & 0xF0) == 0xE0) b += 3;
+        else if ((ch & 0xF8) == 0xF0) b += 4;
+        else b += 1;
+        c++;
+    }
+    return b;
+}
+
+size_t utf8_strlen(const char *s) {
+    size_t count = mbstowcs(NULL, s, 0);
+    return (count == (size_t)-1) ? strlen(s) : count;
+}
 
 int main(int argc, char *argv[]) {
     setlocale(LC_ALL, ""); // Sistem Türkçe/UTF-8 karakter kümesini aktif eder
@@ -54,6 +80,12 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    // Eğer dosya tamamen boşsa veya yeni oluşturulduysa kapasiteyi 1 yapıyoruz
+    if (capacity == 0) {
+        capacity = 1;
+        en_uzun_satirin_boyu = 0;
+    }
+
     rewind(fPtr);
 
     char satirsatir[en_uzun_satirin_boyu + 1];
@@ -90,34 +122,137 @@ int main(int argc, char *argv[]) {
         i++;
     }
 
+    // Eğer dosyadan hiçbir şey okunmadıysa (dosya boşsa), yazmaya başlamak için ilk satırı başlatıyoruz
+    if (i == 0) {
+        Metin[0] = strdup("");
+        i = 1;
+    }
+
     rewind(fPtr);
-    
+
     for (int index = 0; index < capacity; index++) {
         mvprintw(index, 0, Metin[index]);
     }
     refresh();
 
-    move(0, 0);
-    y = 0, x = 0;
+    int x, y;
+    x = 0;
+    y = 0;
+    int ch;
+    move(y, x);
 
-    while (ch = getch())
-    {
-        
-        if (ch == KEY_LEFT) {
-            if (x > 0) x--;
-        }
-        else if (ch == KEY_RIGHT) {
-            if (x < satir_uzunlugu_bul(metin, y - 1)) x++;
-        }
-        else if (ch == KEY_UP) {
-            if (y > 0) y--;
-        }
-        else if (ch == KEY_DOWN) {
-            if (y < metnin_satir_sayisi(metin) + 2) y++;
-        }
-    }
+    while ((ch = getch()) != 3) {
     
+        switch (ch) {
+            case CTRL_S:
+                // Dosya boyutunu 0 bayt yapıp içeriği temizler
+                ftruncate(fileno(fPtr), 0); 
+                // Yazma imlecini dosyanın en başına çeker
+                fseek(fPtr, 0, SEEK_SET);
 
+                // Bellekteki güncel satırları dosyaya yazıyoruz
+                for (int j = 0; j < capacity; j++) {
+                    fputs(Metin[j], fPtr);
+                    fputs("\n", fPtr);
+                }
+                fflush(fPtr); // Tam yazmayı garanti etmek için tamponu boşaltıyoruz
+
+                // Ekranın alt satırına kaydedildi bildirimi basıyoruz
+                mvprintw(LINES - 1, COLS/2, "[ Kaydedildi ]");
+                refresh();
+                break;
+            case KEY_UP:
+                if (y > 0) y--;
+                break;
+            case KEY_DOWN:
+                if (y < capacity - 1) y++;
+                break;
+            case KEY_LEFT:
+                if (x > 0) x--;
+                break;
+            case KEY_RIGHT:
+                if (x < utf8_strlen(Metin[y])) x++;
+                break;
+            case KEY_ENTER:
+            case '\n':
+            case '\r': {
+                // 1. Metin pointer dizisinde 1 yeni satır için bellek genişletiyoruz
+                char **temp = realloc(Metin, (capacity + 1) * sizeof(char *));
+                if (temp != NULL) {
+                    Metin = temp;
+
+                    // 2. y+1 ve altındaki satır pointer'larını 1 adım aşağı kaydırıyoruz
+                    memmove(&Metin[y + 2], &Metin[y + 1], (capacity - (y + 1)) * sizeof(char *));
+
+                    // 3. Yeni satıra boş bir string atıyoruz
+                    Metin[y + 1] = strdup("");
+
+                    capacity++;
+                    i++;
+
+                    // 4. İmleci yeni satırın başına çekiyoruz
+                    y++;
+                    x = 0;
+
+                    // 5. Ekrandaki tüm satırları yeniden çizip tazeliyoruz
+                    clear();
+                    for (int index = 0; index < capacity; index++) {
+                        mvprintw(index, 0, "%s", Metin[index]);
+                    }
+                    refresh();
+                }
+                break;
+            }
+            case KEY_BACKSPACE:
+            case 127:       // Çoğu Linux terminalinde Backspace tuşunun ASCII karşılığı
+            case '\b': 
+                if (x > 0) {
+                    int b_curr = char_to_byte(Metin[y], x);
+                    int b_prev = char_to_byte(Metin[y], x - 1);
+                    int total_bytes = strlen(Metin[y]);
+
+                    // Karakterin bellekte kapladığı tüm baytları (örn. 2 bayt) tek seferde siliyoruz
+                    memmove(Metin[y] + b_prev, Metin[y] + b_curr, total_bytes - b_curr + 1);
+                    x--;
+
+                    mvaddstr(y, 0, Metin[y]);
+                    clrtoeol();
+                }
+                break;
+
+            default:
+                // Yazdırılabilir tuşlar ve UTF-8 baytları
+                if (ch >= 32 && ch != 127) {
+                    int b_curr = char_to_byte(Metin[y], x);
+                    int total_bytes = strlen(Metin[y]);
+
+                    // Bellekte yeni bayt için yer açıp ekliyoruz
+                    memmove(Metin[y] + b_curr + 1, Metin[y] + b_curr, total_bytes - b_curr + 1);
+                    Metin[y][b_curr] = ch;
+
+                    // Gelen bayt UTF-8 devam baytı değilse (yeni bir karakter başlığıysa) imleç sütununu 1 artır
+                    if ((ch & 0xC0) != 0x80) {
+                        x++;
+                    }
+
+                    mvaddstr(y, 0, Metin[y]);
+                    clrtoeol();
+                }
+                break;  
+        }
+
+        // alta veya üste giderken imlecimiz böşlukta görünmesin
+        
+        if (x > utf8_strlen(Metin[y])) {
+            x = utf8_strlen(Metin[y]);
+        }
+        
+        move(y, x);
+
+        
+    }
+
+    
 
     for (int index = 0; index < i; index++) {
         free(Metin[index]);
@@ -126,6 +261,7 @@ int main(int argc, char *argv[]) {
     fclose(fPtr);
 
     endwin();
+    printf("hhhh");
 
     return 0;    
 }
@@ -138,3 +274,8 @@ int main(int argc, char *argv[]) {
             move(y + 1, 0); // Y'yi 1 artır (alt satır), X'i 0 yap (en baş)
         }
             */
+
+
+
+    // index noktasından string sonuna kadar olan kısmı (len - index + 1 bayt) 
+    // 1 adım sağa (str + index + 1) kaydırıyoruz ('\0' dahil kayar)
